97294616a20d8daf042efdd4d2663bb499f2987b
kANG JONGHUN <2424kang@tukorea.ac.kr>
2025-10-16 07:32:08 +0900

    대략적인 기본 틀 handle_event, reset_world, update_world, render_world 선언

---
+++ princess_rescue.py
@@ -0,0 +1,35 @@
+from pico2d import *
+import random
+
+
+
+
+
+running=True
+
+
+
+
+def handle_event():
+    pass
+
+
+def reset_world():
+    pass
+
+
+def update_world():
+    pass
+
+
+def render_world():
+    pass
+
+open_canvas()
+reset_world()
+while running:
+    handle_event()
+    reset_world()
+    update_world()
+    render_world()
+
