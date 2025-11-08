43c443c81e5e111d4d74870606537464a71c0a1a
kangjonghun030218 <2424kang@tukorea.ac.kr>
2025-11-08 23:06:03 +0900

    메뉴 설정 했지만 화면에 나오지 않음

--- princess_rescue.py
+++ princess_rescue.py
@@ -5,7 +5,7 @@ CANVAS_WIDTH = 800
 CANVAS_HEIGHT = 600
 
 cam_x, cam_y = 0, 0
-game_mode = 'play'
+game_mode = 'menu'
 knight = None
 game_map = None
 
@@ -14,14 +14,18 @@ world = []
 
 
 class GameMap:
-    def __init__(self, map_number=1):
+    def __init__(self, map_number=0):
+        self.map0_image=load_image('map_0.png')
         self.map1_image = load_image('map_1.png')
         self.map2_image = load_image('map_2.png')
         self.map_number = map_number
 
-        if self.map_number == 1:
+
+        if self.map_number == 0:
+            self.image=self.map0_image
+        elif self.map_number == 1:
             self.image = self.map1_image
-        else:
+        elif self.map_number == 2:
             self.image = self.map2_image
 
         self.width = self.image.w
@@ -30,9 +34,9 @@ class GameMap:
         self.world_y = self.height // 2
 
     def draw(self, cam_x, cam_y):
-        screen_x = self.world_x - cam_x
-        screen_y = self.world_y - cam_y
-        self.image.draw(screen_x, screen_y)
+            screen_x = self.world_x - cam_x
+            screen_y = self.world_y - cam_y
+            self.image.draw(screen_x, screen_y)
 
     def update(self):
         pass
@@ -341,6 +345,9 @@ def handle_event():
         elif event.type == SDL_KEYDOWN:
             if event.key == SDLK_ESCAPE:
                 running = False
+            elif event.key == SDLK_0:
+                game_mode = 'menu'
+                reset_world(0)
             elif event.key == SDLK_1:
                 game_mode = 'play'
                 reset_world(1)
