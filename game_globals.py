a446d1d638ab0eaaa51de39bd50ab57126b909f5
kangjonghun030218 <2424kang@tukorea.ac.kr>
2025-11-15 20:25:37 +0900

    몬스터를 처치하는 퀘스트를 위해서 quest_log 수정

--- game_globals.py
+++ game_globals.py
@@ -18,6 +18,12 @@ quest_log = {
     'check_npc': {
         'status': 'not_started',
         'talked_to_man': False
+    },
+    'monster_hunt': {
+        'status': 'not_started',
+        'total_quest_stage': 1,
+        'current_target_type': 1,
+        'current_kill_count': 0
     }
 }
 
