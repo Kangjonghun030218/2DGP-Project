2ecd6cadb35a2e499b7363cd334f56b7d126992d
kangjonghun030218 <2424kang@tukorea.ac.kr>
2025-11-15 21:50:31 +0900

    포션 class 구현 완료

---
+++ potion.py
@@ -0,0 +1,30 @@
+from pico2d import *
+import game_globals as g
+
+class Potion:
+    def __init__(self, x, y, potion_type):
+        self.world_x = x
+        self.world_y = y
+        self.potion_type = potion_type
+
+        if self.potion_type == 'hp':
+            self.image = g.hp_potion_image
+        else:
+            self.image = g.mp_potion_image
+
+        self.width = self.image.w
+        self.height = self.image.h
+
+    def draw(self, cam_x, cam_y):
+        screen_x = self.world_x - cam_x
+        screen_y = self.world_y - cam_y
+        self.image.draw(screen_x, screen_y)
+
+    def get_bb(self):
+        half_width = self.width / 2
+        half_height = self.height / 2
+        return (self.world_x - half_width, self.world_y - half_height,
+                self.world_x + half_width, self.world_y + half_height)
+
+    def update(self):
+        pass
