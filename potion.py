74fa14c86316ecfea4ae6d056f35c71988608574
kangjonghun030218 <2424kang@tukorea.ac.kr>
2025-11-15 21:52:27 +0900

    포션 크기 조정 완료

--- potion.py
+++ potion.py
@@ -18,7 +18,7 @@ class Potion:
     def draw(self, cam_x, cam_y):
         screen_x = self.world_x - cam_x
         screen_y = self.world_y - cam_y
-        self.image.draw(screen_x, screen_y)
+        self.image.draw(screen_x, screen_y,20,20)
 
     def get_bb(self):
         half_width = self.width / 2
