1b607e9742eeb0eb3111980c6618c84c9ee2e3ed
kangjonghun030218 <2424kang@tukorea.ac.kr>
2025-11-16 19:41:13 +0900

    서버 모듈 구현

---
+++ server.py
@@ -0,0 +1,19 @@
+knight = None
+game_map = None
+world = []
+
+quest_log = {
+    'check_npc': {
+        'status': 'not_started',
+        'talked_to_man': False
+    },
+    'monster_hunt': {
+        'status': 'not_started',
+        'total_quest_stage': 1,
+        'current_target_type': 1,
+        'current_kill_count': 0
+    }
+}
+dialogue_message = None
+
+cam_x, cam_y = 0, 0
