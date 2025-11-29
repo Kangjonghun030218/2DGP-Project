d2e65f6e0f7b688629ba5ca4402b07331ccdf32c
kANG JONGHUN <2424kang@tukorea.ac.kr>
2025-11-29 21:55:38 +0900

    npc, 포탈, 보스몹도 미니맵에 뜨게 구현 완료

--- map_view_mode.py
+++ map_view_mode.py
@@ -6,6 +6,9 @@ import server
 import config
 import menu_mode
 from monster import Monster
+from portal import Portal
+from boss import Boss
+from npc import NPC
 
 
 class MapViewState(BaseState):
@@ -27,10 +30,42 @@ class MapViewState(BaseState):
                                        config.CANVAS_HEIGHT)
 
             for obj in server.world:
+
                 if isinstance(obj, Monster) and obj.current_hp > 0:
                     mx = (obj.world_x1 / server.game_map.width) * config.CANVAS_WIDTH
                     my = (obj.world_y1 / server.game_map.height) * config.CANVAS_HEIGHT
                     obj.images['idle'].clip_draw(0, 192, 64, 64, mx, my, 50, 50)
+
+                elif isinstance(obj, Portal):
+                    mx = (obj.x / server.game_map.width) * config.CANVAS_WIDTH
+                    my = (obj.y / server.game_map.height) * config.CANVAS_HEIGHT
+                    if obj.image:
+                        obj.image.draw(mx, my, 40, 40)
+
+                elif isinstance(obj, Boss) and obj.current_hp > 0:
+                    mx = (obj.x / server.game_map.width) * config.CANVAS_WIDTH
+                    my = (obj.y / server.game_map.height) * config.CANVAS_HEIGHT
+
+                    if obj.body_image:
+                        obj.body_image.draw(mx, my, 100, 100)
+
+                elif isinstance(obj, NPC):
+                    mx1 = (obj.world_x1 / server.game_map.width) * config.CANVAS_WIDTH
+                    my1 = (obj.world_y1 / server.game_map.height) * config.CANVAS_HEIGHT
+                    obj.image1.clip_draw(0, 0, 48, 48, mx1, my1, 40, 40)
+
+                    mx2 = (obj.world_x2 / server.game_map.width) * config.CANVAS_WIDTH
+                    my2 = (obj.world_y2 / server.game_map.height) * config.CANVAS_HEIGHT
+                    obj.image2.clip_draw(0, 0, 48, 48, mx2, my2, 40, 40)
+
+                    mx3 = (obj.world_x3 / server.game_map.width) * config.CANVAS_WIDTH
+                    my3 = (obj.world_y3 / server.game_map.height) * config.CANVAS_HEIGHT
+                    obj.image3.clip_draw(0, 0, 48, 48, mx3, my3, 40, 40)
+
+                    mx4 = (obj.world_x4 / server.game_map.width) * config.CANVAS_WIDTH
+                    my4 = (obj.world_y4 / server.game_map.height) * config.CANVAS_HEIGHT
+                    obj.image4.clip_draw(0, 0, 48, 48, mx4, my4, 40, 40)
+
             if server.knight:
                 screen_x = (server.knight.world_x / server.game_map.width) * config.CANVAS_WIDTH
                 screen_y = (server.knight.world_y / server.game_map.height) * config.CANVAS_HEIGHT
