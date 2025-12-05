3c6ed05f5fe1104a2658397cd54148ab7edd91b9
kangjonghun030218 <2424kang@tukorea.ac.kr>
2025-12-05 17:45:28 +0900

    퀘스트 알림이 npc위에 둥둥 뜨게 구현 완료

--- npc.py
+++ npc.py
@@ -10,6 +10,9 @@ class NPC:
         self.image2 = resource_manager.get_image('npc_old_man')
         self.image3 = resource_manager.get_image('npc_man')
         self.image4 = resource_manager.get_image('npc_boy')
+        self.icon_start = resource_manager.get_image('quest_start')
+        self.icon_ing = resource_manager.get_image('quest_ing')
+        self.icon_end = resource_manager.get_image('quest_end')
         self.frame = 0
 
         self.world_x1, self.world_y1 = 1540, 480
@@ -32,17 +35,14 @@ class NPC:
         if dist_to_1 < self.talk_range:
             server.dialogue_message = "안녕하세요? 날씨가 참 좋네요. 조심해서 다니세요."
             return True
-
         elif dist_to_2 < self.talk_range:
             server.dialogue_message = "허허, 젊은이 반갑구만. 마을 밖은 위험하다네."
             return True
-
         elif dist_to_3 < self.talk_range:
             monster_names = {
                 1: "초록 오크", 2: "파란 오크", 3: "강한 초록 오크",
                 4: "초록 슬라임", 5: "파란 슬라임", 6: "불타오르는 슬라임"
             }
-
             current_stage = hunt_quest['total_quest_stage']
             target_name = monster_names.get(current_stage, f"monster (type {current_stage})")
 
@@ -60,14 +60,16 @@ class NPC:
                     if hunt_quest['total_quest_stage'] > 6:
                         server.dialogue_message = "고마워 모든 미션을 클리어 해줬어! 이제 길을 떠나도 좋아."
                         hunt_quest['status'] = 'completed'
-
                         new_portal = Portal(1290, 884, 'map3')
                         server.world.append(new_portal)
+                        hunt_quest['reward_claimed'] = True
+
                     else:
                         hunt_quest['current_target_type'] = hunt_quest['total_quest_stage']
                         hunt_quest['current_kill_count'] = 0
                         next_target_name = monster_names.get(hunt_quest['current_target_type'], "next monster")
                         server.dialogue_message = f"고마워! 다음은 {next_target_name} 10마리를 처치해줘."
+
                 else:
                     remaining = 10 - hunt_quest['current_kill_count']
                     server.dialogue_message = f"이봐 아직 {target_name} {remaining}마리를 더 퇴치해야하네."
@@ -92,6 +94,25 @@ class NPC:
         self.image2.clip_draw(self.frame * 48, 0, 48, 48, screen_x2, screen_y2)
         self.image3.clip_draw(self.frame * 48, 0, 48, 48, screen_x3, screen_y3)
         self.image4.clip_draw(self.frame * 48, 0, 48, 48, screen_x4, screen_y4)
+
+        hunt_quest = server.quest_log['monster_hunt']
+        q_status = hunt_quest['status']
+        kill_count = hunt_quest['current_kill_count']
+        current_icon = None
+
+        if q_status == 'not_started':
+            current_icon = self.icon_start
+        elif q_status == 'in_progress':
+            if kill_count < 10:
+                current_icon = self.icon_ing
+            else:
+                current_icon = self.icon_end
+
+        if current_icon:
+            floating_y = math.sin(get_time() * 5.0) * 5
+            icon_x = screen_x3
+            icon_y = screen_y3 + 50 + floating_y
+            current_icon.draw(icon_x, icon_y, 40, 40)
 
     def update(self, frame_time):
         self.frame_timer += frame_time
